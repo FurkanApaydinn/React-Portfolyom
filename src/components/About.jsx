c9fdb0fa8515669f15241a66a961f4bcd8654731
Furkan Apaydin <furkanapydn@gmail.com>
2023-01-08 13:11:35 +0300

    responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
+// import logo from './İmages/logo4.png'
+// import logo2 from './İmages/logo.png'
+// import jslogo from './İmages/jslogo.png'
+// import foto from './İmages/FurkanApaydın.jpg'
+// import logogit from './İmages/logogit.png'
+import '..//App.css'
+
 export default function About() {
   return (
     <div>
-      
+
     </div>
   )
 }
